605af5fd0385957c35e0fdb41ed7f300b930bbcd
prstk2 <146347114+prstk2@users.noreply.github.com>
2023-10-06 23:43:25 +0300

    at least they do render properly

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ running = True
 
 
 class Checker(sprite.Sprite):
-    def __init__(self, pos_x, pos_y, color, img, image_scale, king=0):
+    def __init__(self, pos_x, pos_y, color, img, img_scale, king=0):
         super().__init__()
         self.isking = king
         self.color = color
-        """        self.image = transform.scale(image.load(img), (img_scale))
+        self.image = transform.scale(image.load(img), (img_scale, img_scale))
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y """
+        self.rect.x = pos_x
+        self.rect.y = pos_y
 
     def move(self):
         pass
@@ -31,21 +31,22 @@ matrix = [[0, 1, 0, 1, 0, 1, 0, 1],
           [2, 0, 2, 0, 2, 0, 2, 0]]
 
 checker_group = sprite.Group()
-for row in matrix:
-    for column in row:
-        if column == 1:
-            peice = Checker(matrix[column], matrix[row], 'white', '', 64)
-        if column == 2:
-            peice = Checker(matrix[column], matrix[row], 'black', '', 64)
-    checker_group.add(peice)
+for row in range(len(matrix)):
+    for column in range(len(matrix[row])):
+        if matrix[row][column] == 1:
+            piece = Checker(column*64, row*64, 'white', 'white.png', 64)
+            checker_group.add(piece)
+        if matrix[row][column] == 2:
+            piece = Checker(column*64, row*64, 'black', 'black.png', 64)
+            checker_group.add(piece)
 
-print(checker_group)
+
 while running:
     for e in event.get():
         if e.type == QUIT:
             running = False  
-    window.fill((0, 0, 0))
-
+    window.blit(image.load('board.png'), (0, 0))
+    checker_group.draw(window)
 
     clock.tick()
     display.update()
